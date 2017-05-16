d734c6c8d9b3a386c93d1b1c84a7954a634c5a37
Shruti Priya <shrutipriya1808@gmail.com>
2017-05-16 15:41:11 -0700

    create an interface repository class IPieRepository

--- src/ShrutiPieShop/Models/Category.cs
+++ src/ShrutiPieShop/Models/Category.cs
@@ -7,5 +7,9 @@ namespace ShrutiPieShop.Models
 {
     public class Category
     {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Pie> Pies { get; set; }
     }
 }
