121791e7e0069fe9a7ff429fa87d640652316e58
Shruti Priya <shrutipriya1808@gmail.com>
2017-05-16 16:15:45 -0700

    add DBSet of Pie and Category to the AppDbContext

--- src/ShrutiPieShop/Models/AppDbContext.cs
+++ src/ShrutiPieShop/Models/AppDbContext.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ShrutiPieShop.Models
 {
-    public class AppDbContext
+    public class AppDbContext :DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+        //public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
